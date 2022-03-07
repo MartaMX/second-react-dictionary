841b65adc083e54c927a7baf761611669f57561f
MartaMX <92443035+MartaMX@users.noreply.github.com>
2022-03-07 20:54:20 +0100

    REACT Search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='text-center App-footer'>
+        <small>Coded by MartaMX</small>
+      </footer>
     </div>
   );
 }
-
-export default App;
